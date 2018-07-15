4212ca085e9732a87c894f62080de42d02910e77
Amie <sunburntkamel@users.noreply.github.com>
2018-07-15 00:04:28 -0700

    add tile layer

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
 var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox.streets',
+    accessToken: 'pk.eyJ1IjoiYW1pZWZyZWV0bHkiLCJhIjoiZThmZjNlN2M4ODM0ZDVlYjYyYzg4Y2NkNjZhZDE4ZjYifQ.lm_wjUUEtLvBo6OA6w38vw'
+}).addTo(mymap);
